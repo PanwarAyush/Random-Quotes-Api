ed69ba97d2b1cc41417e7419d597d56c13c6fe0a
Ayush Panwar <ayushpanwar691@gmail.com>
2023-01-19 09:34:53 +0530

    now with 76,000 Quotes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const cors=require('cors')
 const app = express()
-const q=require('./quotes.json')
+const quotes=require('./Quotes_75966.json')
 app.use(cors())
 app.use(express.json())
 
 app.get('/',(req,res)=>{
-let ind=(Math.random()*1664)|0
-  res.send(q[ind])
+let ind=(Math.random()*75966)|0
+  res.send(quotes[ind])
 })
 app.listen(8080,()=>{
   console.log('Server started at http://localhost:8080')
